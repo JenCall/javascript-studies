f8002f6676d08de480b653ae6c4827f320d3fe58
Jeniffer Callou <jenifferprc@gmail.com>
2023-02-24 16:20:40 +0100

    Add closures and scope notes 2

--- scope-and-closures/scope-and-closures.js
+++ scope-and-closures/scope-and-closures.js
@@ -71,4 +71,20 @@ const size16 = makeSizer(16);
 
 // 1. Local scope (Own scope)
 // 2. Enclosing scope (can be block, function, or module scope)
-// 2. Global scope
+// 3. Global scope
+
+// global scope
+const e = 10;
+function sum(a) {
+  return function (b) {
+    return function (c) {
+      // outer functions scope
+      return function (d) {
+        // local scope
+        return a + b + c + d + e;
+      };
+    };
+  };
+}
+
+console.log(sum(1)(2)(3)(4)); // 20
