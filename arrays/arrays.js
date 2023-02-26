833ffbede78df83ef0db03ba630a6a7087828fa9
Jeniffer Callou <jenifferprc@gmail.com>
2023-02-26 18:35:47 +0100

    Add array push method

---
+++ arrays/arrays.js
@@ -0,0 +1,6 @@
+
+// The push() method in JavaScript is used to add one or more elements to the end of an array.
+let fruits = ['apple', 'banana', 'orange'];
+fruits.push('grape');
+
+console.log(fruits); // Output: ['apple', 'banana', 'orange', 'grape']
